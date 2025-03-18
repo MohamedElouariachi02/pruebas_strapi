bd8ce938d8daf467d9f7e86a1bf615d46e27c218
Magickio <dad2dadad@gmail.com>
2025-03-18 11:59:19 +0000

    Conexiones api

--- Main.js
+++ Main.js
@@ -5,6 +5,12 @@ let ejercicios = []
 const api = new API('http://localhost:1337/api/');
 let exercise = api.getExercisesById(2);
 
+api.getExercises().then(exercises => {
+    for (let exercise of exercises.data) {
+        console.log(exercise.name);
+    }
+})
+
 exercise.then(info => {
     let exer = new Exercise(info.data[0].name, info.data[0].description);
     ejercicios.push(exer);
